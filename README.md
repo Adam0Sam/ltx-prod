78997ca6ce90251b6bbc201808450edc148a2c43
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2024-01-04 02:35:57 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# LTX Website
+
+A simple UI demonstrating the objectives of the LTX team and their progress
+
+## How to setup
+
+```
+# Clone this repository
+$ git clone ...
+
+# Go into the repository
+$ cd ltx-prod
+
+# Install dependencies
+$ npm install
+
+# Bundle the code for production
+$ npx webpack
+```
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  rules: [
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: [["@babel/preset-env", { targets: "defaults" }]],
+        },
+      },
+    },
+  ],
+};
