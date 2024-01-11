071fd2cb1b73b3021a825da9763476991dbd1372
Adam0Sam <132478985+Adam0Sam@users.noreply.github.com>
2024-01-11 20:08:26 +0200

    Background ball animation

--- js/index.js
+++ js/index.js
@@ -10,9 +10,51 @@ const loadMouseFollower = () => {
 // #todo: add event listeners for the scroll so that scroll-snap works smoothly
 const loadEventListeners = () => {};
 
+const loadBallAnimation = () => {
+  const colors = ["#ff000080", "#ff77005e", "#ff0000", "#ff7700", "#930000"];
+  const numSections = document.querySelectorAll("section").length;
+  const avgBallSize = 10; // in rem
+  // number of balls present in the page
+  const numBalls = 100;
+  const balls = [];
+
+  for (let i = 0; i < numBalls; i++) {
+    const ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+    ball.style.left = `${Math.floor(Math.random() * 100 * numSections)}vw`;
+    ball.style.top = `${Math.floor(Math.random() * 100 * numSections)}vh`;
+    ball.style.width = `${Math.random() * avgBallSize}rem`;
+    ball.style.height = ball.style.width;
+
+    balls.push(ball);
+    document.body.append(ball);
+  }
+
+  const animationDuration = 2000; // (miliseconds)
+  // Keyframes
+  const yDisplacement = 110; // in rem
+  const xDisplacement = 110; // in rem
+  balls.forEach((ball, id) => {
+    const to = {
+      x: Math.random() * (id % 2 === 0 ? -xDisplacement : xDisplacement),
+      y: Math.random() * yDisplacement,
+    };
+
+    ball.animate([{ transform: "translate(0, 0)" }, { transform: `translate(${to.x}rem, ${to.y}rem)` }], {
+      duration: (Math.random() + 1) * animationDuration, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out",
+    });
+  });
+};
+
 const loadPage = () => {
   loadMouseFollower();
   loadEventListeners();
+  loadBallAnimation();
 };
 
 document.addEventListener("DOMContentLoaded", loadPage);
